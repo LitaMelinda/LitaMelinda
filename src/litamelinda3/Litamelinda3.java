d2525932ba5a7160b5259f39fd65ff0ad5f20a8a
MYP <sword.arch@yahoo.co.id>
2014-10-08 14:02:10 +0700

    first commit

---
+++ src/litamelinda3/Litamelinda3.java
@@ -0,0 +1,26 @@
+
+package litamelinda3;
+
+
+public class Litamelinda3 {
+
+    
+    public static void main(String[] args) {
+       
+
+        String nama="Lita Melinda";
+         System.out.println("Nama:"+nama);
+        String npm="13311122";
+         System.out.println("Npm:"+npm);
+         String kelas="SI 13E";
+         System.out.println("Kelas:"+kelas);
+        String jurusan="SI";
+         System.out.println("Jurusan:"+jurusan);
+        String kampus="Teknokrat";
+         System.out.println("kampus:"+kampus);
+    }
+    
+}
+    }
+    
+}
